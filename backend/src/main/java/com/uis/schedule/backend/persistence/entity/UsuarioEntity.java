33a9c029f86947016febb86280448e981f97d610
RicardoJaimes04 <svensson2704@gmail.com>
2025-06-25 18:22:44 -0500

    Fix: fix an error on Docker
    fix the error and add the principal class (user)

---
+++ backend/src/main/java/com/uis/schedule/backend/persistence/entity/UsuarioEntity.java
@@ -0,0 +1,19 @@
+package com.uis.schedule.backend.persistence.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long id;
+
+    private String nombre;
+    private String correo;
+
+
+}
+
