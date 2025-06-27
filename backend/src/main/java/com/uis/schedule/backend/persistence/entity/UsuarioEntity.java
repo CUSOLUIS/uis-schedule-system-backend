303d92e7fc2a03b1b75416e5fc68608a106dc464
RicardoJaimes04 <svensson2704@gmail.com>
2025-06-26 19:11:12 -0500

    implementation: implements a basic spring Security config

--- backend/src/main/java/com/uis/schedule/backend/persistence/entity/UsuarioEntity.java
+++ backend/src/main/java/com/uis/schedule/backend/persistence/entity/UsuarioEntity.java
@@ -8,7 +8,7 @@ import jakarta.persistence.*;
 public class UsuarioEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nombre;
--- backend/src/main/java/com/uis/schedule/backend/presentation/controller/Prueba.java
+++ backend/src/main/java/com/uis/schedule/backend/presentation/controller/Prueba.java
@@ -2,13 +2,19 @@ package com.uis.schedule.backend.presentation.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
 public class Prueba {
 
     @GetMapping("/hola")
     public String prueba(){
         return "Hola mundo";
+    }
+    @GetMapping("/hola-secured")
+    public String pruebaSecured(){
+        return "Hola mundo secured";
     }
 }
