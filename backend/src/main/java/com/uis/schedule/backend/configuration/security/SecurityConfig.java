17a46fdde44b9af2eb93ed5bbd8a463a2f60fc69
RicardoJaimes04 <svensson2704@gmail.com>
2025-06-27 21:10:38 -0500

    Security: Implements basic security configurations for credentials
    New funtionalities like annotations in controller methods

--- backend/src/main/java/com/uis/schedule/backend/configuration/security/SecurityConfig.java
+++ backend/src/main/java/com/uis/schedule/backend/configuration/security/SecurityConfig.java
@@ -4,16 +4,26 @@ package com.uis.schedule.backend.configuration.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -23,8 +33,21 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.build();
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session  -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//VERSION WITHOUT ANNOTATIONS
+//                .authorizeHttpRequests(http -> {
+//                    http.requestMatchers(HttpMethod.GET, "/auth/hola").permitAll();
+//                    http.requestMatchers(HttpMethod.GET, "/auth/hola-secured").hasAnyAuthority("READ");
+//                    http.anyRequest().denyAll();
+//                } )
+                .build();
     }
+
+
+
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -35,9 +58,28 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider provider= new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(null);
-        provider.setUserDetailsService(null);
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
         return provider;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        List<UserDetails> userDetailsList = new ArrayList<>();
+
+        userDetailsList.add(User.withUsername("richard")
+                .password("1234")
+                .roles("ADMIN")
+                .authorities("READ","CREATE")
+                .build());
+        userDetailsList.add(User.withUsername("Marcos")
+                .password("1234")
+                .roles("USER")
+                .authorities("READ")
+                .build());
+
+
+        return new InMemoryUserDetailsManager(userDetailsList);
     }
 
     @Bean
--- backend/src/main/java/com/uis/schedule/backend/presentation/controller/Prueba.java
+++ backend/src/main/java/com/uis/schedule/backend/presentation/controller/Prueba.java
@@ -1,20 +1,32 @@
 package com.uis.schedule.backend.presentation.controller;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@PreAuthorize("denyAll()")
 public class Prueba {
 
     @GetMapping("/hola")
+    @PreAuthorize("permitAll()")
     public String prueba(){
         return "Hola mundo";
     }
+
+
     @GetMapping("/hola-secured")
+    @PreAuthorize("hasAuthority('READ')")
     public String pruebaSecured(){
         return "Hola mundo secured";
+    }
+
+    @GetMapping("/hola-secured2")
+
+    public String pruebaSecured2(){
+        return "Hola mundo secured2";
     }
 }
