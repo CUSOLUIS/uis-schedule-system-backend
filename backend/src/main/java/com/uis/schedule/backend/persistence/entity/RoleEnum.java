5e9fa99db4fa9656cd4786f3f3675c34e38de0f7
RicardoJaimes04 <svensson2704@gmail.com>
2025-06-29 22:36:00 -0500

    add: add security entities

---
+++ backend/src/main/java/com/uis/schedule/backend/persistence/entity/RoleEnum.java
@@ -0,0 +1,9 @@
+package com.uis.schedule.backend.persistence.entity;
+
+public enum RoleEnum {
+
+    ADMIN,
+    USER,
+    INVITED,
+    DEVELOPER
+}
--- backend/src/main/java/com/uis/schedule/backend/persistence/entity/UsuarioEntity.java
+++ backend/src/main/java/com/uis/schedule/backend/persistence/entity/UsuarioEntity.java
@@ -10,13 +10,14 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "usuarios")
+@Table(name = "users")
 public class UsuarioEntity {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_usuario")
-    private Long idUsuario;
+
+    private Long id;
 
     @Column(name = "email", nullable = false, unique = true, length = 100)
     private String email;
@@ -24,18 +25,17 @@ public class UsuarioEntity {
     @Column(name = "password", nullable = false, length = 255)
     private String password;
 
-    @Column(name = "activo")
-    private boolean activo;
+    @Column(name = "enabled")
+    private boolean enabled;
 
-    @Column(name = "cuenta_no_expirada")
-    private boolean cuentaNoExpirada;
+    @Column(name = "account_non_expired")
+    private boolean accountNonExpired;
 
-    @Column(name = "cuenta_no_bloqueada")
-    private boolean cuentaNoBloqueada;
+    @Column(name = "account_non_locked")
+    private boolean accountNonLocked;
 
-    @Column(name = "credenciales_no_expiradas")
-    private boolean credencialesNoExpiradas;
-
-
+    @Column(name = "credentials_non_expired")
+    private boolean credentialsNonExpired;
 }
+
 
