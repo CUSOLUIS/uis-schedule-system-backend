611b8e8ecd915a6e5129034f05c031c07a322d51
Marcos Nitola <marcos123.mdnr@gmail.com>
2025-09-15 18:50:56 -0500

    refactor(mvc-base): update mapper for new DTO fields

--- backend/src/main/java/com/uis/schedule/backend/util/mapper/UserMapper.java
+++ backend/src/main/java/com/uis/schedule/backend/util/mapper/UserMapper.java
@@ -9,6 +9,24 @@ public class UserMapper {
 		dto.setId(entity.getUserId());
 		dto.setName(entity.getName());
 		dto.setEmail(entity.getEmail());
+		dto.setPassword(entity.getPassword());
+		dto.setRole(entity.getRole());
+		dto.setPermissions(entity.getPermissions());
+		dto.setActive(entity.isActive());
+		dto.setLastSession(entity.getLastSession());
 		return dto;
+	}
+
+	public static UserEntity dtoToEntity(UserDTO dto){
+		UserEntity entity = new UserEntity();
+		entity.setUserId(dto.getId());
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+		entity.setPassword(dto.getPassword());
+		entity.setRole(dto.getRole());
+		entity.setPermissions(dto.getPermissions());
+		entity.setActive(dto.isActive());
+		entity.setLastSession(dto.getLastSession());
+		return entity;
 	}
 }
